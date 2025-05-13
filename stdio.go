b311ade133254f7d87921e1c59781f367ffb18d9
panorama32 <63278411+panorama32@users.noreply.github.com>
2025-05-13 15:34:38 +0900

    implementation: simple

---
+++ stdio.go
@@ -0,0 +1,27 @@
+package simplemcp
+
+import "os"
+
+func newStdioReadWriteCloser() *stdioReadWriteCloser {
+	return &stdioReadWriteCloser{
+		in:  os.Stdin,
+		out: os.Stdout,
+	}
+}
+
+type stdioReadWriteCloser struct {
+	in  *os.File
+	out *os.File
+}
+
+func (s *stdioReadWriteCloser) Read(p []byte) (int, error) {
+	return s.in.Read(p)
+}
+
+func (s *stdioReadWriteCloser) Write(p []byte) (int, error) {
+	return s.out.Write(p)
+}
+
+func (s *stdioReadWriteCloser) Close() error {
+	return nil
+}
